5aaf6059a2b89fba7a6b5364905137dcdbf94da8
GUILLERMO VALENZUELA <kaoscore53@yahoo.com>
2023-10-14 15:30:57 -0500

    Add project files.

---
+++ BuilderExample/PersonBuilder.cs
@@ -0,0 +1,34 @@
+﻿namespace BuilderExample;
+public class PersonBuilder
+{
+    private Person person = new Person();
+
+    public PersonBuilder SetName(string name)
+    {
+        person.Name = name;
+        return this;
+    }
+
+    public PersonBuilder SetAge(int age)
+    {
+        person.Age = age;
+        return this;
+    }
+
+    public PersonBuilder SetAddress(string address)
+    {
+        person.Address = address;
+        return this;
+    }
+
+    public PersonBuilder SetEmail(string email)
+    {
+        person.Email = email;
+        return this;
+    }
+
+    public Person Build()
+    {
+        return person;
+    }
+}
---
+++ BuilderExample/Program.cs
@@ -0,0 +1,8 @@
+﻿using BuilderExample;
+
+Person person = new PersonBuilder()
+    .SetName("John Doe")
+    .SetAge(30)
+    .SetAddress("123 Main Street")
+    .SetEmail("john@example.com")
+    .Build();
